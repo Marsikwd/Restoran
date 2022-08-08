a94484c9907c04971ad5c13167a65a8b2ca3a37e
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-08-08 17:25:33 +0300

    first-commit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ if (menu && menuBtn) {
 	})
 }
 
+
+var t;
+
+function up() {
+	var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+	if (top > 0) {
+		window.scrollBy(0, ((top + 100) / -10));
+		t = setTimeout('up()', 20);
+	} else clearTimeout(t);
+	return false;
+}
+window.addEventListener('scroll', function () {
+	arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+});
