1f9e1760779ab7b1e99e28aa4c9c5894895cb242
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-08-08 20:23:53 +0300

    First-commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-// butger
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.burg_menu');
-const body = document.body;
 
-if (menu && menuBtn) {
-	menuBtn.addEventListener('click', () => {
-		menu.classList.toggle('active')
-	})
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu_link");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
 
+hamburger.addEventListener("click", toggleMenu);
 
 var t;
 
